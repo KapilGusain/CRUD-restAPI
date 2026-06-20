ead4ef0857c2ccdc230250ec18d136377277c1c3
KapilGusain <kapilgusain20@gmail.com>
2026-06-20 12:52:42 +0530

    updated users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
 var mongoose= require('mongoose');
 
-mongoose.connect('mongodb://kapilgusain20_db_user:Xz6I9HbxgNzTB5f1@ac-yubdjyk-shard-00-00.0pscnzn.mongodb.net:27017,ac-yubdjyk-shard-00-01.0pscnzn.mongodb.net:27017,ac-yubdjyk-shard-00-02.0pscnzn.mongodb.net:27017/?ssl=true&replicaSet=atlas-136fwg-shard-0&authSource=admin&appName=PracticeDB');
+mongoose.connect('mongodb://kapilgusain20_db_user:password@ac-yubdjyk-shard-00-00.0pscnzn.mongodb.net:27017,ac-yubdjyk-shard-00-01.0pscnzn.mongodb.net:27017,ac-yubdjyk-shard-00-02.0pscnzn.mongodb.net:27017/?ssl=true&replicaSet=atlas-136fwg-shard-0&authSource=admin&appName=PracticeDB');
 
-// Xz6I9HbxgNzTB5f1
 
 const userSchema = mongoose.Schema({
   name: String,
